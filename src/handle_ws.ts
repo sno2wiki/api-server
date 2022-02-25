bf67c5ea47d80c0e690b4286606599393f1672ce
SnO2WMaN <me@sno2wman.net>
2022-02-26 04:22:02 +0900

    handle_wsの切り出し

---
+++ src/handle_ws.ts
@@ -0,0 +1,85 @@
+import { deleteTicket, findDoc, findTicket, updateDocValue } from "./mongo/mod.ts";
+
+const socketsMap = new Map<string, Set<WebSocket>>();
+const valMap = new Map<string, unknown[]>();
+
+export const handleWS = (ws: WebSocket, { documentId }: { documentId: string }) => {
+  if (!socketsMap.has(documentId)) socketsMap.set(documentId, new Set());
+
+  let userId: string | null = null;
+
+  ws.addEventListener("open", () => {
+    socketsMap.get(documentId)!.add(ws);
+  });
+
+  ws.addEventListener("message", async (event) => {
+    const data = JSON.parse(event.data);
+    switch (data.type) {
+      case "ENTER":
+        {
+          /* authen */
+          try {
+            const { ticket } = data;
+            const result = await findTicket(ticket);
+            if (!result) {
+              ws.send(JSON.stringify({ type: "FORCE_EXIT" }));
+              return;
+            }
+            userId = result.userId;
+            await deleteTicket(ticket);
+          } catch (err) {
+            ws.send(JSON.stringify({ type: "FORCE_EXIT" }));
+            return;
+          }
+
+          /* stored  */
+          if (valMap.has(documentId)) {
+            ws.send(JSON.stringify({ type: "PULL_VAL", value: (valMap.get(documentId)!), userId }));
+            return;
+          }
+
+          try {
+            const stored = await findDoc(documentId);
+            if (stored && stored.value) {
+              const value = stored.value;
+              valMap.set(documentId, value);
+              ws.send(JSON.stringify({ type: "PULL_VAL", value: value, userId }));
+            } else {
+              const value = [{ "type": "paragraph", children: [{ text: "" }] }];
+              valMap.set(documentId, value);
+              ws.send(JSON.stringify({ type: "PULL_VAL", value: value, userId }));
+            }
+          } catch (err) {
+            ws.send(JSON.stringify({ type: "FORCE_EXIT" }));
+            return;
+          }
+        }
+        break;
+      case "PUSH_VAL":
+        {
+          if (!userId) {
+            ws.send(JSON.stringify({ type: "FORCE_EXIT" }));
+            return;
+          }
+
+          const { value } = data;
+          const sendData = JSON.stringify({ type: "PULL_VAL", value, userId });
+          socketsMap.get(documentId)!.forEach((to) => {
+            if (ws === to || to.readyState !== WebSocket.OPEN) return;
+            to.send(sendData);
+          });
+          valMap.set(documentId, value);
+        }
+        break;
+    }
+  });
+
+  ws.addEventListener("close", async () => {
+    socketsMap.get(documentId)!.delete(ws);
+
+    const value = valMap.get(documentId);
+    if (userId && value) {
+      await updateDocValue(documentId, { value, userId });
+    }
+  });
+};
--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,11 @@ import { bold, yellow } from "std/fmt/colors";
 import { Application, Router } from "oak";
 import { Bson } from "mongo";
 import { oakCors } from "cors";
-import { deleteTicket, findDoc, findTicket, publishTicket, updateDocValue } from "./mongo/mod.ts";
+import { publishTicket } from "./mongo/mod.ts";
+import { handleWS } from "./handle_ws.ts";
 
 const app = new Application();
 const router = new Router();
-
-const socketsMap = new Map<string, Set<WebSocket>>();
-const valMap = new Map<string, unknown[]>();
-
 export const extractBearerToken = (authorization: string): string | null => {
   if (
     authorization.startsWith("Bearer ") ||
@@ -19,86 +16,6 @@ export const extractBearerToken = (authorization: string): string | null => {
   } else {
     return null;
   }
-};
-export const handleWS = (ws: WebSocket, { documentId }: { documentId: string }) => {
-  if (!socketsMap.has(documentId)) socketsMap.set(documentId, new Set());
-
-  let userId: string | null = null;
-
-  ws.addEventListener("open", () => {
-    socketsMap.get(documentId)!.add(ws);
-  });
-
-  ws.addEventListener("message", async (event) => {
-    const data = JSON.parse(event.data);
-    switch (data.type) {
-      case "ENTER":
-        {
-          /* authen */
-          try {
-            const { ticket } = data;
-            const result = await findTicket(ticket);
-            if (!result) {
-              ws.send(JSON.stringify({ type: "FORCE_EXIT" }));
-              return;
-            }
-            userId = result.userId;
-            await deleteTicket(ticket);
-          } catch (err) {
-            ws.send(JSON.stringify({ type: "FORCE_EXIT" }));
-            return;
-          }
-
-          /* stored  */
-          if (valMap.has(documentId)) {
-            ws.send(JSON.stringify({ type: "PULL_VAL", value: (valMap.get(documentId)!), userId }));
-            return;
-          }
-
-          try {
-            const stored = await findDoc(documentId);
-            if (stored && stored.value) {
-              const value = stored.value;
-              valMap.set(documentId, value);
-              ws.send(JSON.stringify({ type: "PULL_VAL", value: value, userId }));
-            } else {
-              const value = [{ "type": "paragraph", children: [{ text: "" }] }];
-              valMap.set(documentId, value);
-              ws.send(JSON.stringify({ type: "PULL_VAL", value: value, userId }));
-            }
-          } catch (err) {
-            ws.send(JSON.stringify({ type: "FORCE_EXIT" }));
-            return;
-          }
-        }
-        break;
-      case "PUSH_VAL":
-        {
-          if (!userId) {
-            ws.send(JSON.stringify({ type: "FORCE_EXIT" }));
-            return;
-          }
-
-          const { value } = data;
-          const sendData = JSON.stringify({ type: "PULL_VAL", value, userId });
-          socketsMap.get(documentId)!.forEach((to) => {
-            if (ws === to || to.readyState !== WebSocket.OPEN) return;
-            to.send(sendData);
-          });
-          valMap.set(documentId, value);
-        }
-        break;
-    }
-  });
-
-  ws.addEventListener("close", async () => {
-    socketsMap.get(documentId)!.delete(ws);
-
-    const value = valMap.get(documentId);
-    if (userId && value) {
-      await updateDocValue(documentId, { value, userId });
-    }
-  });
 };
 
 router.get("/docs/:id/edit", async (context) => {
