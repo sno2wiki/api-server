35e113f1d2eefa0a125408626631c7c068452d67
SnO2WMaN <me@sno2wman.net>
2022-02-27 01:47:36 +0900

    collection

--- src/mongo.ts
+++ src/mongo.ts
@@ -3,12 +3,13 @@ import { Bson, MongoClient } from "mongo";
 const mongoClient = new MongoClient();
 await mongoClient.connect(Deno.env.get("MONGO_URI")!);
 
+const ticketsCollection = mongoClient.database().collection("tickets");
 const docsCollection = mongoClient.database().collection("docs");
 
 export const publishTicket = async (docId: string, userId: string) => {
   const ticket = crypto.randomUUID();
 
-  await mongoClient.database().collection("tickets").insertOne({
+  await ticketsCollection.insertOne({
     ticket: ticket,
     documentId: docId,
     userId: userId,
@@ -18,40 +19,33 @@ export const publishTicket = async (docId: string, userId: string) => {
   return { ticket };
 };
 export const findTicket = (ticket: string) => {
-  return mongoClient
-    .database()
-    .collection("tickets")
-    .findOne({ ticket }, { projection: { "userId": true } });
+  return ticketsCollection.findOne(
+    { ticket },
+    { projection: { "userId": true } },
+  );
 };
 export const deleteTicket = (ticket: string) => {
-  return mongoClient
-    .database()
-    .collection("tickets")
-    .deleteOne({ ticket });
+  return ticketsCollection.deleteOne(
+    { ticket },
+  );
 };
 
 export const findDoc = (docId: string) => {
-  return mongoClient
-    .database()
-    .collection("docs")
-    .findOne(
-      { _id: new Bson.ObjectId(docId) },
-      { projection: { "value": true } },
-    );
+  return docsCollection.findOne(
+    { _id: new Bson.ObjectId(docId) },
+    { projection: { "value": true } },
+  );
 };
 
 export const updateDocValue = (docId: string, { value, userId }: { value: unknown[]; userId: string }) => {
-  return mongoClient
-    .database()
-    .collection("docs")
-    .updateOne(
-      { _id: new Bson.ObjectId(docId) },
-      {
-        $set: { value: (value) },
-        $addToSet: { editors: userId },
-      },
-      { upsert: true },
-    );
+  return docsCollection.updateOne(
+    { _id: new Bson.ObjectId(docId) },
+    {
+      $set: { value: (value) },
+      $addToSet: { editors: userId },
+    },
+    { upsert: true },
+  );
 };
 
 export const findRedirects = (context: string | null, term: string) => {
